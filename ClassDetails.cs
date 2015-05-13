cd8774a44f1833e64c2ca51b733cfea8fb7d48cd
shreedhar <shreedhar.patil072@gmail.com>
2015-05-13 12:40:31 +0100

    Create ClassDetails.cs

---
+++ ClassDetails.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace roslyncompiler
+{
+    public class ClassDetails
+    {
+        public string ClassName { get; set; }
+
+        public string NameSpace { get; set; }
+    }
+}
